d5924150aee0c641ec59fd8c3622d7a4ac75a49a
BilalITPro <ba3864163@gmail.com>
2025-12-11 00:16:24 +0500

    Initial project commit - HTML CSS JS website

---
+++ script.js
@@ -0,0 +1,10 @@
+// Simple alert button on home page
+function showMessage() {
+    alert("Welcome! This is JavaScript working on this webpage.");
+}
+
+// Contact form alert
+function formSubmit(event) {
+    event.preventDefault();
+    alert("Your message has been sent!");
+}
